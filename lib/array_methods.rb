277f442cce3d15a8011312f270ad4a8ec2fc0c86
Laila Campbell <lailacampbell96@gmail.com>
2020-06-05 03:08:39 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -2,7 +2,7 @@ def using_include(array, element)
 array = ["wow", "I", "am", "really", "learning", "arrays!"]
     # element1 = "wow"
     # element2 = "Tardis"
-    array.include?("wow",Tardis")
+    array include?("wow",Tardis")
 end
 
 def using_sort(array)
